0bfb21eab2d28da9dc935a6e0af2f00273abc529
Azizan <azmenezi@gmail.com>
2023-09-09 17:00:34 +0300

    added messing libraries

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { LogBox } from "react-native";
+import "react-native-gesture-handler";
+import { LogBox, StatusBar } from "react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import UserContext from "./src/contexts/UserContext";
 import { useState } from "react";
@@ -7,6 +8,7 @@ import BottomNavigation from "./src/navigations/BottomNav";
 
 LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
+StatusBar.setBarStyle("light-content", true);
 
 export default function App() {
   const [user, setUser] = useState(null);
@@ -14,7 +16,6 @@ export default function App() {
   return (
     <QueryClientProvider client={new QueryClient()}>
       <UserContext.Provider value={{ user, setUser }}>
-        {/* <NavigationContainer theme={isDarkMode ? DarkTheme : LightTheme}> */}
         <NavigationContainer theme={DarkTheme}>
           {/* <SafeAreaView> */}
           <BottomNavigation />
